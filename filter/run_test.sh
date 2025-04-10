e70cafafcfe1bd10efee4b3e3c5f73300e2a4056
apo11o <iliabondar04@yandex.ru>
2025-04-11 01:09:23 +0300

    optimized structure

--- filter/run_test.sh
+++ filter/run_test.sh
@@ -1,4 +1,4 @@
 for i in {1..10}
 do
-  python3 url_test.py configs${i}.txt -o output${i}.txt --singbox-path /usr/local/bin/sing-box
+  python3 url_test.py input/configs${i}.txt -ao output/output${i}.txt -w 5 --singbox-path /usr/local/bin/sing-box
 done
