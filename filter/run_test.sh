81e2d0d98682353ed5d11016b4d7dc413d476f57
apo11o <iliabondar04@yandex.ru>
2025-04-10 22:10:41 +0300

    initial

--- filter/run_test.sh
+++ filter/run_test.sh
@@ -1,4 +1,4 @@
 for i in {1..10}
 do
-  python3 vpn_tester.py configs${i}.txt -o output${i}.txt --singbox-path /usr/local/bin/sing-box
+  python3 url_test.py configs${i}.txt -o output${i}.txt --singbox-path /usr/local/bin/sing-box
 done
